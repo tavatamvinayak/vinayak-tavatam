f7a1e02c5d4655067367e19cf249d84c80897bdb
vinayak tavatam <tavatamvinayak@gmail.com>
2023-04-25 23:56:26 +0530

    projects changes

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,71 +9,49 @@ function projects() {
       <main>
         <h1 className='text-center '>My Projects</h1>
 
-        <div>
-          <div className='bg-gray-500 p-4' >
-            <div className='flex-row'>
-
-              <div className="projects bg-blue-400 p-5">
-                <div><img src="" alt="" /></div>
-                <div className="content">
-                  <h1 className='ProjectTitle'> E-commerce </h1>
-                  <p className="ProjectContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore iure, placeat consectetur</p>
-                </div>
-              </div>
-
-              <div className="projects bg-blue-400 p-5">
-                <div><img src="" alt="" /></div>
-                <div className="content">
-                  <h1 className='ProjectTitle'> E-commerce </h1>
-                  <p className="ProjectContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore iure, placeat consectetur</p>
-                </div>
-              </div>
-
-              <div className="projects bg-blue-400 p-5">
-                <div><img src="" alt="" /></div>
-                <div className="content">
-                  <h1 className='ProjectTitle'> E-commerce </h1>
-                  <p className="ProjectContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore iure, placeat consectetur</p>
-                </div>
-              </div>
-
-              <div className="projects bg-blue-400 p-5">
-                <div><img src="" alt="" /></div>
-                <div className="content">
-                  <h1 className='ProjectTitle'> E-commerce </h1>
-                  <p className="ProjectContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore iure, placeat consectetur</p>
-                </div>
-              </div>
-
-              <div className="projects bg-blue-400 p-5">
-                <div><img src="" alt="" /></div>
-                <div className="content">
-                  <h1 className='ProjectTitle'> E-commerce </h1>
-                  <p className="ProjectContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore iure, placeat consectetur</p>
-                </div>
-              </div>
-
-              <div className="projects bg-blue-400 p-5">
-                <div><img src="" alt="" /></div>
-                <div className="content">
-                  <h1 className='ProjectTitle'> E-commerce </h1>
-                  <p className="ProjectContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore iure, placeat consectetur</p>
-                </div>
-              </div>
-
-              <div className="projects bg-blue-400 p-5">
-                <div><img src="" alt="" /></div>
-                <div className="content">
-                  <h1 className='ProjectTitle'> E-commerce </h1>
-                  <p className="ProjectContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint tempore iure, placeat consectetur</p>
-                </div>
-              </div>
-
-            </div>
-
-
-          </div>
+        <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    
+    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+      <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div className="rounded-lg h-64 overflow-hidden">
+          <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1203x503" />
         </div>
+        <h2 className="text-xl font-medium title-font text-gray-900 mt-5">Shooting Stars</h2>
+        <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+        <a className="text-indigo-500 inline-flex items-center mt-3">Learn More
+          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div className="rounded-lg h-64 overflow-hidden">
+          <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504" />
+        </div>
+        <h2 className="text-xl font-medium title-font text-gray-900 mt-5">The Catalyzer</h2>
+        <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+        <a className="text-indigo-500 inline-flex items-center mt-3">Learn More
+          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+        <div className="rounded-lg h-64 overflow-hidden">
+          <img alt="content" className="object-cover object-center h-full w-full" src="https://dummyimage.com/1205x505" />
+        </div>
+        <h2 className="text-xl font-medium title-font text-gray-900 mt-5">The 400 Blows</h2>
+        <p className="text-base leading-relaxed mt-2">Swag shoivdigoitch literally meditation subway tile tumblr cold-pressed. Gastropub street art beard dreamcatcher neutra, ethical XOXO lumbersexual.</p>
+        <a className="text-indigo-500 inline-flex items-center mt-3">Learn More
+          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
 
 
       </main>
